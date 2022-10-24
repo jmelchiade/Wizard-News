87759d8bff02b0a578e2cf2be01be9104b7bb7b3
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-24 19:24:30 -0400

    worked with Jessie to understand mapping data array for post title and name

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
-// const morgan = require('morgan')
-// app.use(morgan('dev'))
+const morgan = require('morgan');
+const postBank = require("./postBank");
+app.use(morgan('dev'))
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
 
 
+const html = `<!DOCTYPE html>
+<html>
+<head>
+<title> Wizard News</title> 
+</head>
+<body>
+<ul>
+
+${ posts.map(post => `<li>${post.title}, ${post.name})}</li>`)}
+// Review .join() to get rid of commas from list
+</ul>
+</body>
+</html>`;
+
+res.send(html);
+
+})
 
 
 
